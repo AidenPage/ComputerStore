b75a2d3e582977a9e6c01396893ce65d976e33fc
Aiden <aidenpage@gmail.com>
2016-03-03 06:28:51 +0200

    Start

---
+++ Documents/NetBeansProjects/Exercise2/src/main/java/com/aiden/exercise2/Truth.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aiden.exercise2;
+
+/**
+ *
+ * @author 211121614
+ */
+public class Truth {
+    
+    boolean add(boolean choice) {
+        return choice; //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ Documents/NetBeansProjects/Exercise2/src/test/java/com/aiden/exercise2/TruthTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aiden.exercise2;
+
+import junit.framework.Assert;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author 211121614
+ */
+public class TruthTest {
+    public TruthTest (){}
+    
+     @Test
+     public void add() {
+     Truth truth = new Truth();
+     boolean result = truth.add(true);
+     Assert.assertEquals(true, result);
+         
+     }
+    
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
